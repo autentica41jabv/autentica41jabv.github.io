1447c3cddecf6325a46b3ead93b0a4aea3356af9
autentica41jabv <81274074+autentica41jabv@users.noreply.github.com>
2021-03-23 20:19:21 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyBkX3sMokEK-DGk8IKswnoNf47MX_c1fHg",
+    authDomain: "autentifica41jabv.firebaseapp.com",
+    projectId: "autentifica41jabv",
+    storageBucket: "autentifica41jabv.appspot.com",
+    messagingSenderId: "692204945298",
+    appId: "1:692204945298:web:d5ffed2bc83d7289154b4b",
+    measurementId: "G-JDS9HMMVTE"
     });
